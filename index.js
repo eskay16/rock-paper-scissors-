acc1ae24155f29397c00691172fee88647499073
eskay16 <odefemiayomide@gmail.com>
2026-04-16 02:02:13 +0300

    fix: trying to fix prompt not working on github but works on local

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
 console.log("JS loaded");
 
-
-const getComputerChoice = ()=>{
+const getComputerChoice = () => {
     const choice = Math.ceil(Math.random() * 3);
-    return choice === 1? "rock": choice === 2? "paper": "scissors";
+    return choice === 1 ? "rock" : choice === 2 ? "paper" : "scissors";
 }
 
-const getHumanChoice = ()=>{
-
+const getHumanChoice = () => {
     const humanChoice = window.prompt("Enter Rock, paper or scissors", "").toLowerCase();
-
     return humanChoice;
 }
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playGame(){
-let humanScore = 0;
-let computerScore = 0;
-const round = playRound;
+    for (let i = 0; i < 5; i++) {
+         playRound(getHumanChoice(), getComputerChoice());
+    }
 
+    function playRound(humanSelection, computerSelection) {
+        if (humanSelection === computerSelection) return console.log("tie");
 
+        switch (humanSelection) {
+            case "rock":
+                computerSelection === "scissors"
+                    ? (console.log("you win! Rock beats scissors"), humanScore++)
+                    : (console.log("you lose! Paper beats Rock"), computerScore++);
+                break;
 
-for(let i = 0; i < 5; i++){
-    round(getHumanChoice,getComputerChoice);
-}
+            case "paper":
+                computerSelection === "rock"
+                    ? (console.log("you win! paper beats rock"), humanScore++)
+                    : (console.log("you lose! scissors beats paper"), computerScore++);
+                break;
 
+            case "scissors":
+                computerSelection === "paper"
+                    ? (console.log("you win! scissors beats paper"), humanScore++)
+                    : (console.log("you lose! Rock beats Scissors"), computerScore++);
+                break;
 
+            default:
+                console.log("Invalid input");
+        }
+    }
 
-function playRound(humanChoice, computerChoice){
-
-    const humanSelection= humanChoice();
-    const computerSelection = computerChoice();
-
-    if(humanSelection === computerSelection) return console.log("tie");
-
-    switch(humanSelection){
-        case "rock":
-            computerSelection === "scissors"? console.log("you win! Rock beats scissors"): console.log("you lose! Paper beats Rock"); 
-            computerSelection === "scissors"?  humanScore++ : computerScore++;
-            break;
-        case "paper":
-            computerSelection === "rock"? console.log("you win! paper beats rock"): console.log("you lose! scissors beats paper"); 
-            computerSelection === "rock"?  humanScore++ : computerScore++;
-            break;
-        case "scissors":
-            computerSelection === "paper"? console.log("you win! scissors beats paper"): console.log("you lose! Rock beats Scissors"); 
-            computerSelection === "paper"?  humanScore++ : computerScore++;
-            break;
-        default:
-}
-}
-
-return (console.log(humanScore > computerScore? "you win!":humanScore == computerScore?"tie": "you lose!", ` you ${humanScore}: computer ${computerScore}`)
-);
+    console.log(
+        humanScore > computerScore
+            ? "you win!"
+            : humanScore == computerScore
+            ? "tie"
+            : "you lose!",
+        ` you ${humanScore}: computer ${computerScore}`
+    );
 }
 
 playGame();
