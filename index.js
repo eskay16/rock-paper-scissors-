86de0b3898f0ed7b3fa69b0dd8ab16d15049f7e1
eskay16 <odefemiayomide@gmail.com>
2026-04-24 00:09:56 +0300

    feat: added some ui, and also images

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-console.log("JS loaded");
+// console.log("JS loaded");
 
-const getComputerChoice = () => {
-    const choice = Math.ceil(Math.random() * 3);
-    return choice === 1 ? "rock" : choice === 2 ? "paper" : "scissors";
-}
+// const getComputerChoice = () => {
+//     const choice = Math.ceil(Math.random() * 3);
+//     return choice === 1 ? "rock" : choice === 2 ? "paper" : "scissors";
+// }
 
-const getHumanChoice = () => {
-    const humanChoice = window.prompt("Enter Rock, paper or scissors", "").toLowerCase();
-    return humanChoice;
-}
+// const getHumanChoice = () => {
+//     const humanChoice = window.prompt("Enter Rock, paper or scissors", "").toLowerCase();
+//     return humanChoice;
+// }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-         playRound(getHumanChoice(), getComputerChoice());
-    }
+//     for (let i = 0; i < 5; i++) {
+//          playRound(getHumanChoice(), getComputerChoice());
+//     }
 
-    function playRound(humanSelection, computerSelection) {
-        if (humanSelection === computerSelection) return console.log("tie");
+//     function playRound(humanSelection, computerSelection) {
+//         if (humanSelection === computerSelection) return console.log("tie");
 
-        switch (humanSelection) {
-            case "rock":
-                computerSelection === "scissors"
-                    ? (console.log("you win! Rock beats scissors"), humanScore++)
-                    : (console.log("you lose! Paper beats Rock"), computerScore++);
-                break;
+//         switch (humanSelection) {
+//             case "rock":
+//                 computerSelection === "scissors"
+//                     ? (console.log("you win! Rock beats scissors"), humanScore++)
+//                     : (console.log("you lose! Paper beats Rock"), computerScore++);
+//                 break;
 
-            case "paper":
-                computerSelection === "rock"
-                    ? (console.log("you win! paper beats rock"), humanScore++)
-                    : (console.log("you lose! scissors beats paper"), computerScore++);
-                break;
+//             case "paper":
+//                 computerSelection === "rock"
+//                     ? (console.log("you win! paper beats rock"), humanScore++)
+//                     : (console.log("you lose! scissors beats paper"), computerScore++);
+//                 break;
 
-            case "scissors":
-                computerSelection === "paper"
-                    ? (console.log("you win! scissors beats paper"), humanScore++)
-                    : (console.log("you lose! Rock beats Scissors"), computerScore++);
-                break;
+//             case "scissors":
+//                 computerSelection === "paper"
+//                     ? (console.log("you win! scissors beats paper"), humanScore++)
+//                     : (console.log("you lose! Rock beats Scissors"), computerScore++);
+//                 break;
 
-            default:
-                console.log("Invalid input");
-        }
-    }
+//             default:
+//                 console.log("Invalid input");
+//         }
+//     }
 
-    console.log(
-        humanScore > computerScore
-            ? "you win!"
-            : humanScore == computerScore
-            ? "tie"
-            : "you lose!",
-        ` you ${humanScore}: computer ${computerScore}`
-    );
-}
+//     console.log(
+//         humanScore > computerScore
+//             ? "you win!"
+//             : humanScore == computerScore
+//             ? "tie"
+//             : "you lose!",
+//         ` you ${humanScore}: computer ${computerScore}`
+//     );
+// }
 
-playGame();
+// playGame();
